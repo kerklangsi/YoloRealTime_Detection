ecd15cbe49defa5b200454d8f84ee6dafa3b41c3
kerklangsi <arifilah.01@gmail.com>
2025-10-11 13:08:30 +0800

    update

--- yolo.py
+++
@@ -1,5 +0,0 @@
-import roboflow
-
-rf = roboflow.Roboflow(api_key="IxLNMP2xMrPwHszMHG4E")
-model = rf.workspace().project("people-detection-o4rdr").version("11").model
-prediction = model.download()
